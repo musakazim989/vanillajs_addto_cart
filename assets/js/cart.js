8628456dd0c445cee1f7741bd3c15badd5741aa9
musakazim989 <musakazim989@gmail.com>
2023-07-03 20:59:53 +0600

    add products list

---
+++ assets/js/cart.js
@@ -0,0 +1,7 @@
+import products from "./product.js"
+
+const test = () => {
+  console.log(products)
+}
+
+test()
---
+++ assets/js/product.js
@@ -0,0 +1,36 @@
+const products = [
+  {
+    id: 1,
+    title: "Men chekck shirt",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable",
+    image: "../images/1.jpg",
+    price: "78",
+  },
+  {
+    id: 2,
+    title: "Baby cloth",
+    description:
+      " If you are going to use a passage of Lorem Ipsum, you need to ",
+    image: "../images/2.jpg",
+    price: "35",
+  },
+  {
+    id: 3,
+    title: "Women cloth",
+    description:
+      " Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words",
+    image: "../images/3.jpg",
+    price: "50",
+  },
+  {
+    id: 1,
+    title: "Old women cloth",
+    description:
+      "The standard chunk of Lorem Ipsum used since the 1500s is reproduced",
+    image: "../images/4.jpg",
+    price: "88",
+  },
+]
+
+export default products
