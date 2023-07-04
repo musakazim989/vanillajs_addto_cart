aff1a2b93afbb52d256b917cc8a2540c6d9d7e59
musakazim989 <musakazim989@gmail.com>
2023-07-04 14:51:47 +0600

    bug found

--- assets/js/showProducts.js
+++ assets/js/showProducts.js
@@ -1,13 +1,18 @@
 import products from "./product.js"
 const cards = document.querySelector(".cards")
 
+const addToCart = (id) => {
+  console.log(id)
+}
+
 let myProduct = () => {
   products.map((item, key) => {
-    console.log(item)
     let cardsDiv = document.createElement("div")
     cardsDiv.innerHTML = `
     <div class="card me-4" style="width: 18rem">
-    <img src='../../assets/images/${item.image}' class="card-img-top" alt="..." />
+    <img src='../../assets/images/${
+      item.image
+    }' class="card-img-top" alt="..." />
         <div class="card-body">
           <h5 class="card-title">${item.title}</h5>
           <p class="card-text">
@@ -17,10 +22,10 @@ let myProduct = () => {
             Price: <span>$${item.price}</span>
           </h5>
 
-          <button class="btn btn-primary">Add to Cart</button>
-          <button class="btn btn-primary" disabled>
-            Added
-          </button>
+          <button onclick="addToCart(${
+            (item.id, item.key)
+          })" class="btn btn-primary">Add to Cart</button>
+         
         </div>
       </div>
       `
